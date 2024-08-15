3bfa90ad94df9f3e0b8b0dc56c913d1886db3ecd
sarahebadi <ebadi.sara227@gmai.com>
2024-08-16 02:54:54 +0330

    Refactor UiState.

--- app/src/main/java/com/aparat/androidinterview/UiState.kt
+++ app/src/main/java/com/aparat/androidinterview/UiState.kt
@@ -2,6 +2,6 @@ package com.aparat.androidinterview
 
 sealed class UiState<out T : Any?> {
     data class Success<out T : Any?>(val value: T) : UiState<T>()
-    data class Error(val errorObject: ErrorObject? = null) : UiState<Nothing>()
-    object Loading : UiState<Nothing>()
+    data class Error(val message: String? = null) : UiState<Nothing>()
+    data object Loading : UiState<Nothing>()
 }
